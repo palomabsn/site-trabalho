033d92f37bad2b0b91fae9943e3e58690273ea77
Pah <palomabsn@gmail.com.br>
2025-09-01 20:53:18 -0300

    mobile

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
 // Menu Mobile
 const mobileMenu = document.getElementById('mobileMenu');
-const menu = document.getElementById('menu').querySelector('ul');
+const menu = document.getElementById('menu');
 
 mobileMenu.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
 
-// Fechar menu ao clicar em um link
-menu.querySelectorAll('a').forEach(a => {
-    a.addEventListener('click', () => {
-        menu.classList.remove('show');
-    });
-});
-
 // Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({ behavior: 'smooth' });
+        if(menu.classList.contains('show')) {
+            menu.classList.remove('show');
         }
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });

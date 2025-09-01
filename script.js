bd972f9452df55a1d62998d9eb884ced82ce0777
Pah <palomabsn@gmail.com.br>
2025-09-01 20:30:42 -0300

    mobile

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const mobileMenu = document.getElementById('mobile-menu');
-const menuTopo = document.getElementById('menu-topo');
-
-mobileMenu.addEventListener('click', () => {
-  menuTopo.classList.toggle('active');
-});
+  // Menu Mobile
+        const mobileMenu = document.getElementById('mobileMenu');
+        const menu = document.getElementById('menu');
+        
+        mobileMenu.addEventListener('click', () => {
+            menu.classList.toggle('show');
+        });
+        
+        // Smooth Scrolling
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                
+                if (menu.classList.contains('show')) {
+                    menu.classList.remove('show');
+                }
+                
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });

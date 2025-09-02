2721ff6201145b8f09d36a56813c23e5c1a18d13
Pah <palomabsn@gmail.com.br>
2025-09-01 21:01:12 -0300

    mobile

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-// Menu Mobile
-const mobileMenu = document.getElementById('mobileMenu');
-const menu = document.getElementById('menu');
-
-mobileMenu.addEventListener('click', () => {
-    menu.classList.toggle('show');
-});
-
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        if(menu.classList.contains('show')) {
-            menu.classList.remove('show');
-        }
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+  // Menu Mobile
+        const mobileMenu = document.getElementById('mobileMenu');
+        const menu = document.getElementById('menu');
+        
+        mobileMenu.addEventListener('click', () => {
+            menu.classList.toggle('show');
         });
-    });
-});
+        
+        // Smooth Scrolling
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                
+                if (menu.classList.contains('show')) {
+                    menu.classList.remove('show');
+                }
+                
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
